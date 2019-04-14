ceb8c0f8d60c9018dd4f1940e642f4c093dc8545
anjali-rao <raoanjali0211@gmail.com>
2019-04-14 13:06:52 +0530

    adding home page

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -3,7 +3,10 @@ from userprofile import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', views.login_page),
-    url(r'^login_user', views.login_user),
-    url(r'^signup', views.signup),
+    url(r'^$', views.home, name="home"),
+
+    url(r'^login$', views.login_page, name="login"),
+    url(r'^login_user$', views.login_user, name="login_user"),
+    url(r'^signup$', views.signup, name="signup"),
+    url(r'^logout$', views.logout_user, name="logout"),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,22 +1,13 @@
 from django.http.response import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 from .utils import auth_user
 
 def login_page(request):
     return render(request, "login.html")
-
-def home(request):
-    pass
-
-def login_user(request):
-    #take user name and login, create new if does not exist
-
-    username = request.POST.get('username', '')
-    resp = auth_user(request, username)
-
-    return JsonResponse(resp)
 
 def signup(request):
     #Creates a record for a new user.
@@ -36,6 +27,24 @@ def signup(request):
     resp = auth_user(request, username)
 
     return JsonResponse(resp)
+
+def login_user(request):
+    #take user name and login, create new if does not exist
+
+    username = request.POST.get('username', '')
+    resp = auth_user(request, username)
+
+    return JsonResponse(resp)
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
+
+@login_required
+def home(request):
+
+    context = dict(user=request.user)
+    return render(request, "home.html", context);
 
 def display_profile():
     pass
